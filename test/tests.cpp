7c57c2b372e920ae670fa9597bd97faf4f84d5c1
Vagrant Gautam <me@dippedrusk.com>
2026-05-14 23:50:36 +0200

    test breakpoints

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,6 +3,8 @@
 #include <libvdb/error.hpp>
 #include <libvdb/pipe.hpp>
 #include <libvdb/bit.hpp>
+#include <regex>
+#include <elf.h>
 #include <sys/types.h>
 #include <signal.h>
 #include <fstream>
@@ -22,6 +24,61 @@ namespace {
 		auto index_of_last_parenthesis = data.rfind(')');
 		auto index_of_status_indicator = index_of_last_parenthesis + 2;
 		return data[index_of_status_indicator];
+	}
+
+	std::int64_t get_section_load_bias(std::filesystem::path path, Elf64_Addr file_address) {
+		auto command = std::string("readelf -WS ") + path.string();
+		auto pipe = popen(command.c_str(), "r");
+
+		std::regex text_regex(R"(PROGBITS\s+(\w+)\s+(\w+)\s+(\w+))");
+		char* line = nullptr;
+		std::size_t len = 0;
+		while (getline(&line, &len, pipe) != -1) {
+			std::cmatch groups;
+			if (std::regex_search(line, groups, text_regex)) {
+				auto address = std::stol(groups[1], nullptr, 16);
+				auto offset = std::stol(groups[2], nullptr, 16);
+				auto size = std::stol(groups[3], nullptr, 16);
+				if (address <= file_address and file_address < (address + size)) {
+					free(line);
+					pclose(pipe);
+					return address - offset;
+				}
+			}
+			free(line);
+			line = nullptr;
+		}
+		pclose(pipe);
+		vdb::error::send("Could not find section load bias");
+	}
+
+	std::int64_t get_entry_point_offset(std::filesystem::path path) {
+		std::ifstream elf_file(path);
+
+		Elf64_Ehdr header;
+		elf_file.read(reinterpret_cast<char*>(&header), sizeof(header));
+
+		auto entry_file_address = header.e_entry;
+		auto load_bias = get_section_load_bias(path, entry_file_address);
+		return entry_file_address - load_bias;
+	}
+
+	virt_addr get_load_address(pid_t pid, std::int64_t offset) {
+		std::ifstream maps("/proc/" + std::to_string(pid) + "/maps");
+		std::regex map_regex (R"((\w+)-\w+ ..(.). (\w+))");
+
+		std::string data;
+		while (std::getline(maps, data)) {
+			std::smatch groups;
+			std::regex_search(data, groups, map_regex);
+
+			if (groups[2] == 'x') {
+				auto low_range = std::stol(groups[1], nullptr, 16);
+				auto file_offset = std::stol(groups[3], nullptr, 16);
+				return virt_addr(offset - file_offset + low_range);
+			}
+		}
+		vdb::error::send("Could not find load address");
 	}
 }
 
@@ -242,4 +299,45 @@ TEST_CASE("Iterate breakpoint sites", "[breakpoint]") {
 		[addr = 24](auto& site) mutable {
 			REQUIRE(site.address().addr() == addr++);
 		});
+}
+
+TEST_CASE("Breakpoint on address", "[breakpoint]") {
+	bool close_on_exec = false;
+	vdb::pipe channel(close_on_exec);
+
+	auto proc = process::launch("targets/hello_vdb", true, channel.get_write());
+	channel.close_write();
+
+	auto offset = get_entry_point_offset("targets/hello_vdb");
+	//REQUIRE(offset == 24);
+	auto load_address = get_load_address(proc->pid(), offset);
+	//REQUIRE(load_address.addr() == 24);
+
+	proc->create_breakpoint_site(load_address).enable();
+	proc->resume();
+	auto reason = proc->wait_on_signal();
+
+	REQUIRE(reason.reason == process_state::stopped);
+	REQUIRE(reason.info == SIGTRAP);
+	REQUIRE(proc->get_pc() == load_address);
+
+	proc->resume();
+	reason = proc->wait_on_signal();
+
+	REQUIRE(reason.reason == process_state::exited);
+	REQUIRE(reason.info == 0);
+
+	auto data = channel.read();
+	REQUIRE(to_string_view(data) == "Hello, vdb!\n");
+}
+
+TEST_CASE("Remove breakpoint sites", "[breakpoint]") {
+	auto proc = process::launch("targets/run_endlessly");
+	auto& site = proc->create_breakpoint_site(virt_addr{ 13 });
+	proc->create_breakpoint_site(virt_addr{ 17 });
+	REQUIRE(proc->breakpoint_sites().size() == 2);
+
+	proc->breakpoint_sites().remove_by_id(site.id());
+	proc->breakpoint_sites().remove_by_address(virt_addr{ 17 });
+	REQUIRE(proc->breakpoint_sites().empty());
 }
