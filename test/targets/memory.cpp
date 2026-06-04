76d4b04e960b576162acf2fffd755ba95f271aac
Vagrant Gautam <me@dippedrusk.com>
2026-06-04 17:28:51 +0200

    test reading memory

---
+++ test/targets/memory.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <sys/signal.h>
+#include <unistd.h>
+
+int main() {
+	unsigned long long a = 0xcafebabe;
+	auto a_address = &a;
+
+	write(STDOUT_FILENO, &a_address, sizeof(void*));
+	fflush(stdout);
+
+	raise(SIGTRAP);
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -341,3 +341,18 @@ TEST_CASE("Remove breakpoint sites", "[breakpoint]") {
 	proc->breakpoint_sites().remove_by_address(virt_addr{ 17 });
 	REQUIRE(proc->breakpoint_sites().empty());
 }
+
+TEST_CASE("Reading and writing memory", "[memory]") {
+	bool close_on_exec = false;
+	vdb::pipe channel(close_on_exec);
+	auto proc = process::launch("targets/memory", true, channel.get_write());
+	channel.close_write();
+
+	proc->resume();
+	proc->wait_on_signal();
+
+	auto a_pointer = from_bytes<std::uint64_t>(channel.read().data());
+	auto data_vec = proc->read_memory(virt_addr{ a_pointer }, 8);
+	auto data = from_bytes<std::uint64_t>(data_vec.data());
+	REQUIRE(data == 0xcafebabe);
+}
