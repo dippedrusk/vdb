eb396d8a8ba1660172eea9766945911bc138dbb9
Vagrant Gautam <me@dippedrusk.com>
2026-06-20 14:31:15 +0200

    parse string tables

--- include/libvdb/elf.hpp
+++ include/libvdb/elf.hpp
@@ -1,8 +1,12 @@
 #ifndef VDB_ELF_HPP
 #define VDB_ELF_HPP
 
+#include <unordered_map>
+#include <optional>
+#include <string_view>
 #include <filesystem>
 #include <elf.h>
+#include <libvdb/types.hpp>
 
 namespace vdb {
 	class elf {
@@ -16,8 +20,14 @@ namespace vdb {
 			std::filesystem::path path() const { return path_; }
 			const Elf64_Ehdr& get_header() const { return header_; }
 
+			std::string_view get_string(std::size_t index) const;
+			std::string_view get_section_name(std::size_t index) const;
+			std::optional<const Elf64_Shdr*> get_section(std::string_view name) const;
+			span<const std::byte> get_section_contents(std::string_view name) const;
+
 		private:
 			void parse_section_headers();
+			void build_section_map();
 
 			int fd_;
 			std::filesystem::path path_;
@@ -25,6 +35,7 @@ namespace vdb {
 			std::byte* data_;
 			Elf64_Ehdr header_;
 			std::vector<Elf64_Shdr> section_headers_;
+			std::unordered_map<std::string_view, Elf64_Shdr*> section_map_;
 	};
 }
 
