1a8c3e62c390fa957518dcf67f2754ecbbefa6d9
Vagrant Gautam <me@dippedrusk.com>
2026-06-20 13:09:35 +0200

    parse elf header

---
+++ include/libvdb/elf.hpp
@@ -0,0 +1,28 @@
+#ifndef VDB_ELF_HPP
+#define VDB_ELF_HPP
+
+#include <filesystem>
+#include <elf.h>
+
+namespace vdb {
+	class elf {
+		public:
+			elf(const std::filesystem::path& path);
+			~elf();
+
+			elf(const elf&) = delete;
+			elf& operator=(const elf&) = delete;
+
+			std::filesystem::path path() const { return path_; }
+			const Elf64_Ehdr& get_header() const { return header_; }
+
+		private:
+			int fd_;
+			std::filesystem::path path_;
+			std::size_t file_size_;
+			std::byte* data_;
+			Elf64_Ehdr header_;
+	};
+}
+
+#endif
